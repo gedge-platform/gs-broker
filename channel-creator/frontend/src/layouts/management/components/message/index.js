d319c6912ca0e66cecc01022a7c8ab3aa11d6cbc
jinsung-cho <cjs97111@gmail.com>
2023-09-04 10:47:19 +0900

    feat: Add message parent component

--- channel-creator/frontend/src/layouts/management/components/message/index.js
+++ channel-creator/frontend/src/layouts/management/components/message/index.js
@@ -1,108 +1,201 @@
 import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { useLocation } from "react-router-dom";
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import { styled } from "@mui/system";
-// Material Dashboard 2 React example components
-import DashboardLayout from "components/LayoutContainers/DashboardLayout";
-import Message from "layouts/management/components/message";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+
 import axiosInstance from "axiosInstance";
+import CustromEditor from "layouts/management/components/message/components/editor";
+import Option from "layouts/management/components/message/components/option";
 
-// function TabPanel(props) {
-//   const { children, value, index, ...other } = props;
-//   return (
-//     <div
-//       role="tabpanel"
-//       hidden={value !== index}
-//       id={`simple-tabpanel-${index}`}
-//       aria-labelledby={`simple-tab-${index}`}
-//     >
-//       {value === index && (
-//         <Box sx={{ p: 3 }}>
-//           <Typography>{children}</Typography>
-//         </Box>
-//       )}
-//     </div>
-//   );
-// }
+const style = {
+  width: "100%",
+  maxWidth: "100%",
+  bgcolor: "background.paper",
+};
+function Message({ clusterId }) {
+  const [protocol, setProtocol] = useState("pub/sub");
+  const [channel, setChannel] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [metadata, setMetadata] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [editor, setEditor] = useState({
+    msg: { text: "Create Channel" },
+  });
+  const [errorMessage, setErrorMessage] = useState(null);
 
-// TabPanel.propTypes = {
-//   children: PropTypes.node,
-//   index: PropTypes.number.isRequired,
-//   value: PropTypes.number.isRequired,
-// };
-
-// function a11yProps(index) {
-//   return {
-//     id: `simple-tab-${index}`,
-//     "aria-controls": `simple-tabpanel-${index}`,
-//   };
-// }
-function Management() {
-  const [selectedTab, setselectedTab] = useState(0);
-  const [cluster, setCluster] = useState({});
-  const [url, setURL] = useState("");
-  const location = useLocation();
-
-  const onChangeTab = (event, newValue) => {
-    setselectedTab(newValue);
+  const [messages, setMessages] = useState([]);
+  const nodeCard = {
+    width: "100%",
+    height: "30vh",
   };
 
-  const StyledTabs = styled(Tabs)({
-    width: "30%",
-    color: "info",
-    indicatorColor: "info",
-  });
+  const onChangeProtocol = (event) => {
+    setProtocol(event.target.value);
+  };
+  const onChangeChannel = (event) => {
+    setChannel(event.target.value);
+  };
+  const onChangeClientId = (envet) => {
+    setClientId(event.target.value);
+  };
+  const onChangeMetadata = (envet) => {
+    setMetadata(event.target.value);
+  };
+  const onChangeJson = (value) => {
+    let data;
+    try {
+      data = JSON.parse(value);
+      setErrorMessage(null);
+    } catch (error) {
+      setErrorMessage("Please enter valid JSON.");
+    } finally {
+      setEditor({ msg: data });
+    }
+  };
+
+  const handleSendButton = () => {
+    const data = {
+      clusterId: clusterId,
+      protocol: protocol,
+      channel: channel,
+      clientId: clientId,
+      msg: editor,
+      metadata: metadata,
+    };
+    axiosInstance.post("/single/msg", data).then((response) => {
+      setToggle(!toggle);
+    });
+  };
 
   useEffect(() => {
-    const { id } = location.state;
-    axiosInstance
-      .get(`/single/${id}`, {
-        withCredentials: true,
-      })
-      .then((response) => {
-        const body = response.data;
-        setCluster(body);
-        const newURL = "http://" + body.ip + ":" + body.dashboardPort;
-        setURL(newURL);
-      });
-  }, []);
+    axiosInstance.get(`/single/msg/${clusterId}`).then((response) => {
+      if (response.data === null) {
+        setMessages([]);
+      } else {
+        setMessages(response.data);
+      }
+    });
+  }, [clusterId, toggle]);
 
   return (
-    <DashboardLayout>
-      <Box pt={2} sx={{ width: "100%" }}>
-        <Message clusterId={cluster.id} />
-      </Box>
-    </DashboardLayout>
-    // <DashboardLayout>
-    //   <Box pt={2} sx={{ width: "100%" }}>
-    //     <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-    //       <StyledTabs
-    //         value={selectedTab}
-    //         onChange={onChangeTab}
-    //         aria-label="secondary tabs example"
-    //         textColor="secondary"
-    //         indicatorColor="secondary"
-    //       >
-    //         <Tab label="Message" key={"message"} {...a11yProps(0)} />
-    //         <Tab label="State" key={"state"} {...a11yProps(1)} />
-    //       </StyledTabs>
-    //     </Box>
-    //     <div style={{ display: selectedTab === 0 ? "block" : "none" }}>
-    //       <Message clusterId={cluster.id} />
-    //     </div>
-    //     <div
-    //       className="video-responsive"
-    //       style={{ display: selectedTab === 1 ? "block" : "none", height: "90vh", width: "100%" }}
-    //     >
-    //       <iframe src={url} style={{ width: "100%", height: "100%" }}></iframe>
-    //     </div>
-    //   </Box>
-    // </DashboardLayout>
+    <Grid container spacing={6}>
+      <Grid item xs={8}>
+        <CustromEditor
+          editor={editor}
+          onChangeJson={onChangeJson}
+          errorMessage={errorMessage}
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <Option
+          protocol={protocol}
+          channel={channel}
+          clientId={clientId}
+          metadata={metadata}
+          onChangeProtocol={onChangeProtocol}
+          onChangeChannel={onChangeChannel}
+          onChangeClientId={onChangeClientId}
+          onChangeMetadata={onChangeMetadata}
+          handleSendButton={handleSendButton}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Box pt={3} pb={0}>
+          <Card style={nodeCard}>
+            <Box
+              mx={2}
+              mt={-3}
+              py={2}
+              px={2}
+              variant="gradient"
+              sx={{
+                bgcolor: "info.main",
+                borderRadius: "10px",
+              }}
+              borderRadius="lg"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Typography variant="h6" color="#ffffff">
+                HISTORY
+              </Typography>
+            </Box>
+            <Box
+              pt={3}
+              pl={3}
+              sx={{
+                width: "100%",
+                height: 400,
+                bgcolor: "background.paper",
+                overflow: "auto",
+                borderRadius: "10px",
+              }}
+            >
+              <List sx={style} component="nav">
+                {messages.map((msg, index) => (
+                  <React.Fragment key={index}>
+                    <ListItem button>
+                      <ListItemText
+                        disableTypography
+                        primary={
+                          <>
+                            <Typography
+                              component="div"
+                              variant="body2"
+                              gutterBottom
+                            >
+                              <strong>Protocol:</strong> {msg.protocol}
+                            </Typography>
+                            <Typography
+                              component="div"
+                              variant="body2"
+                              gutterBottom
+                            >
+                              <strong>Channel:</strong> {msg.channel}
+                            </Typography>
+                            <Typography
+                              component="div"
+                              variant="body2"
+                              gutterBottom
+                            >
+                              <strong>ClientID:</strong> {msg.clientId}
+                            </Typography>
+                            <Typography
+                              component="div"
+                              variant="body2"
+                              gutterBottom
+                            >
+                              <strong>Metadata:</strong> {msg.metadata}
+                            </Typography>
+                            <Typography
+                              component="div"
+                              variant="body2"
+                              gutterBottom
+                            >
+                              <strong>Msg:</strong>{" "}
+                              {JSON.stringify(msg.msg.msg, null, 2)}
+                            </Typography>
+                          </>
+                        }
+                      />
+                    </ListItem>
+                    <Divider />
+                  </React.Fragment>
+                ))}
+              </List>
+            </Box>
+          </Card>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
-export default Management;
+export default Message;
